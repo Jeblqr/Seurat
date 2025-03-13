7b564c48dbb0d4f56b3b573859fc5943f662bacd
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2025-03-13 20:33:59 +0800

    Create README.md

--- main.r
+++ main.r
@@ -18,63 +18,65 @@ library(patchwork)
 options(future.globals.maxSize = 800 * 1024^2)  # 800 MiB
 
 # 加载数据
-omFAP <- readRDS("data/omFAP.rds")
+scRNAs <- readRDS("data/omFAP.rds")
 
 # 创建Seurat对象
-#omFAP <- CreateSeuratObject(counts = omFAP$counts, project = "omFAP", meta.data = omFAP$meta.data)
+#scRNAs <- CreateSeuratObject(counts = scRNAs$counts, project = "scRNAs", meta.data = scRNAs$meta.data)
 
 # 设置默认Assay为"RNA"
-DefaultAssay(omFAP) <- "RNA"
+DefaultAssay(scRNAs) <- "RNA"
 
 # 计算线粒体基因的百分比
-#omFAP[["percent.mt"]] <- PercentageFeatureSet(omFAP, pattern = "^MT-")
+#scRNAs[["percent.mt"]] <- PercentageFeatureSet(scRNAs, pattern = "^MT-")
 
 # 可视化QC
 dir.create("figures")
 png("figures/VlnPlot.png", width = 800, height = 800)
-VlnPlot(omFAP, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+VlnPlot(scRNAs, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 dev.off()
 png("figures/ScatterPlot.png", width = 800, height = 800)
-plot1 <- FeatureScatter(omFAP, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(omFAP, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 <- FeatureScatter(scRNAs, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(scRNAs, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
 dev.off()
 
 # 标准化
-omFAP <- NormalizeData(omFAP, normalization.method = "LogNormalize", scale.factor = 10000)
+scRNAs <- NormalizeData(scRNAs, normalization.method = "LogNormalize", scale.factor = 10000)
 
 # Feature筛选
-omFAP <- FindVariableFeatures(omFAP, selection.method = "vst", nfeatures = 2000)
+scRNAs <- FindVariableFeatures(scRNAs, selection.method = "vst", nfeatures = 2000)
 
 #确认Features
-top10 <- head(VariableFeatures(omFAP), 10)
+top10 <- head(VariableFeatures(scRNAs), 10)
 
 # 可视化
 png("figures/VariableFeaturePlot.png", width = 800, height = 800)
-plot1 <- VariableFeaturePlot(omFAP)
+plot1 <- VariableFeaturePlot(scRNAs)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 plot1 + plot2
 dev.off()
 
 # Scaling
-all.genes <- rownames(omFAP)
-omFAP <- ScaleData(omFAP, features = all.genes)
+all.genes <- rownames(scRNAs)
+scRNAs <- ScaleData(scRNAs, features = all.genes)
 
 #PCA
-omFAP <- RunPCA(omFAP, features = VariableFeatures(object = omFAP))
+scRNAs <- RunPCA(scRNAs, features = VariableFeatures(object = scRNAs))
 
 #可视化
 png("figures/PCAPlot1.png", width = 800, height = 800)
-VizDimLoadings(omFAP, dims = 1:2, reduction = "pca")
+VizDimLoadings(scRNAs, dims = 1:2, reduction = "pca")
 dev.off()
 png("figures/PCAPlot2.png", width = 800, height = 800)
-DimPlot(omFAP, reduction = "pca") + NoLegend()
+DimPlot(scRNAs, reduction = "pca") + NoLegend()
 dev.off()
 png("figures/PCAPlotHeatmap.png", width = 800, height = 800)
-DimHeatmap(omFAP, dims = 1:15, cells = 500, balanced = TRUE)
+DimHeatmap(scRNAs, dims = 1:15, cells = 500, balanced = TRUE)
 dev.off()
 
 #ElbowPlot
 png("figures/ElbowPlot.png", width = 800, height = 800)
-ElbowPlot(omFAP)
+ElbowPlot(scRNAs)
 dev.off()
+
+#Cluster the cells
